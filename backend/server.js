dae62363f2913b9c75acabf0cdf4c34bc61d5585
Poojan <poojangoyani@gmail.com>
2024-08-08 00:43:42 +0530

    Ready to use

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import UserRouter from "./routes/user.routes.js";
+
+const PORT = process.env.PORT || 3001;
+
+const __dirname = path.resolve();
+dotenv.config();
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors());
+
+app.use("/api/users", UserRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CommonRoutes from "./routes/CommonRoutes";
+
+function App() {
+  return (
+    <div>
+      <CommonRoutes />
+    </div>
+  );
+}
+
+export default App;
